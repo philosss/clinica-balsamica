786835d6ae86a4675a32c7f12cf3b60f2536957f
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-05-08 12:39:39 +0200

    update with code from lecture May 8th, 2017 (index.js, index.html)

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+const _ = require("lodash");
 
 let serverPort = process.env.PORT || 5000;
 
-let petsList = [
-  { id: 0, name: "bau", tag: "dog" },
-  { id: 1, name: "miao", tag: "cat" }
-];
-
-app.get("/pets", function(req, res) {
-  res.send(JSON.stringify(petsList, 0, 4));
-});
+let petsList = require("./petstoredata.json");
 
 app.use(express.static(__dirname + "/public"));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Register REST entry point */
+app.get("/pets", function(req, res) {
+  let start = parseInt(_.get(req, "query.start", 0));
+  let limit = parseInt(_.get(req, "query.limit", 5));
+  let sortby = _.get(req, "query.sort", "none");
+  let result = petsList;
+
+  if (sortby === "age") {
+    result = _.sortBy(result, [
+      function(o) {
+        return (-1) * o.born;
+      }
+    ]);
+  } else if (sortby === "-age") {
+    result = _.sortBy(result, [
+      function(o) {
+        return o.born;
+      }
+    ]);
+  }
+  result = _.slice(result, start, start + limit);
+  res.send(JSON.stringify(result, 0, 4));
+});
+
+app.post("/pets", function(req, res) {
+  let toappend = {
+    id: petsList.length,
+    name: req.body.name,
+    tag: req.body.tag,
+    born: req.body.born
+  };
+  petsList.push(toappend);
+  res.send(toappend);
+});
+
+app.use(function(req, res) {
+  res.status(400);
+  res.send({ error: "400", title: "404: File Not Found" });
+});
+
 app.set("port", serverPort);
 
+/* Start the server on port 3000 */
 app.listen(serverPort, function() {
   console.log(`Your app is ready at port ${serverPort}`);
 });
