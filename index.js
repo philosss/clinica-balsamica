c51a6006c853edc0e23bd039994bf824b9699c36
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-05-29 20:17:31 +0200

    from psql client to psql pool

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const pg = require('pg');
+const url = require('url');
 
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect(function(err) {
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+
+const config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+};
+const pool = new pg.Pool(config);
+pool.connect(function(err) {
     if (err) {
-        return console.error('error fetching client from client', err);
+        return console.error('error fetching client from pool', err);
     }
 });
 
@@ -16,6 +28,13 @@ app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/locations", function(req, res) {
+    client.query("SELECT * FROM locations", function(err, result) {
+        if (err) { throw err; }
+        return JSON.stringify(result.rows);
+    });
+});
 
 app.set("port", serverPort);
 app.listen(serverPort, function() {
