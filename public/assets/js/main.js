9027c49e73bb27be9156bde9b3a358d9ca011433
Filippo Calzavara <filippo@calzavara.me>
2017-05-29 18:17:11 +0200

    API tests

---
+++ public/assets/js/main.js
@@ -0,0 +1,37 @@
+console.log("Startup");
+
+var data = [
+    {firstname : "Malcom", lastname: "Reynolds"},
+    {firstname : "Kaylee", lastname: "Frye"},
+    {firstname : "Jayne", lastname: "Cobb"}
+];
+
+
+
+/*function showAll(what){
+	/*fetch("/${what}")
+	.then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      document.getElementById("list").innerHTML = data.map(getFullName);
+    //});
+}*/
+
+
+function showAll(what) {
+	var list=$("#list")
+	switch(what) {
+		case "doctors":
+			list.append(data.map(formatDoctors));
+		break;
+	}
+}
+
+function formatDoctors(item,index) {
+    return [item.firstname,item.lastname].join(" ");
+}
+
+
+
+
