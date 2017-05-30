2ceeb63430403c6aaaa0eb6776a539a4eb6ce575
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-05-30 22:22:56 +0200

    Reformatting

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,18 +1,12 @@
 /* ----------------- DATASET FOR TESTING -----------------*/
 
 
-var data = [
-    {firstname : "Malcom", lastname: "Reynolds"},
-    {firstname : "Kaylee", lastname: "Frye"},
-    {firstname : "Jayne", lastname: "Cobb"}
-];
-
-
 
 /* ----------------- API FORMATS -----------------*/
 
 function formatDoctors(item) {
-    return '<div class="card">Nome: '+item.firstname+'<br>Cognome: '+item.lastname+'</div>';
+    console.log(item);
+    return '<div class="card">Nome: ' + item.firstname + '<br>Cognome: ' + item.lastname + '</div>';
 }
 
 
@@ -25,38 +19,38 @@ function formatDoctors(item) {
 
 /* ----------------- NAV JS -----------------*/
 
-$(document).ready(function(){
-	$('#nav-menu').click(function(){
-		$('#nav-menu').toggleClass('open');
-    $('.nav-resp').toggleClass('open');
-	});
+$(document).ready(function() {
+    $('#nav-menu').click(function() {
+        $('#nav-menu').toggleClass('open');
+        $('.nav-resp').toggleClass('open');
+    });
 });
 
 
 
 /* ----------------- API -----------------*/
-function show(what) {
-	var levels=what.split("/");
-	var level1=levels[0];
-	/*fetch("/"+what)
-	.then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {*/
-    var list=$("#list");
-	switch(level1) {
-		case "locations":
-			list.append(data.map(formatLocations));
-		break;
-		case "services":
-			list.append(data.map(formatServices));
-		break;
-		case "doctors":
-			list.append(data.map(formatDoctors));
-		break;
-		default:
-			console.log("NoSuchParameter");
-	}
-    //});
+function show(what, callback) {
+    var levels = what.split("/");
+    var level1 = levels[0];
+    fetch("/" + what)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var list = $("#list");
+            switch (level1) {
+                case "locations":
+                    list.append(data.map(formatLocations));
+                    break;
+                case "services":
+                    list.append(data.map(formatServices));
+                    break;
+                case "doctors":
+                    list.append(data.map(formatDoctors));
+                    break;
+                default:
+                    console.log("NoSuchParameter");
+            }
+        });
 
 }
