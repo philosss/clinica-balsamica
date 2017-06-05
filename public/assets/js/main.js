e30c90262db3f1d87322534195fe1cb265fb9cf1
Filippo Calzavara <filippo@calzavara.me>
2017-06-05 13:27:34 +0200

    Fatto lo stile per All services-doctors

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,275 +1,46 @@
 /* ----------------- DATASET FOR TESTING -----------------*/
 
 
-
-var data = [ 
-  {
-    "id": 1,
-    "name": "Riccardo ",
-    "surname": "Agosta",
-    "email": "r.agosta@balsami.ca",
-    "phonenumber": "02 20399188",
-    "office": 11,
-    "location": 1,
-    "image": "dottori/r.agosta.jpg"
-  },
-    {
-    "id": 2,
-    "name": "Cristina",
-    "surname": "D'ampezzo",
-    "email": "c.dampezzo@balsami.ca",
-    "phonenumber": "02 20310288",
-    "office": 1,
-    "location": 2,
-    "image": "dottori/c.dampezzo.jpg"
-  },
-    {
-    "id": 3,
-    "name": "Gianmaria",
-    "surname": "Barmbilla",
-    "email": "g.brambilla@balsami.ca",
-    "phonenumber": "02 72273989",
-    "office": 23,
-    "location": 2,
-    "image": "dottori/g.brambilla.jpg"
-  },
-    {
-    "id": 4,
-    "name": "Cristoforo",
-    "surname": "Monferro",
-    "email": "c.monferro@balsami.ca",
-    "phonenumber": "02 28282829",
-    "office": 3,
-    "location": 4,
-    "image": "dottori/c.monferro.jpg"
-  },
-    {
-    "id": 5,
-    "name": "Hul",
-    "surname": "Sign",
-    "email": "h.sign@balsami.ca",
-    "phonenumber": "02 101019296",
-    "office": 9,
-    "location": 3,
-    "image": "dottori/h.sign.jpg"
-  },
-    {
-    "id": 6,
-    "name": "Sara",
-    "surname": "Domenica",
-    "email": "s.domenica@balsami.ca",
-    "phonenumber": "02 1920384790",
-    "office": 2,
-    "location": 1,
-    "image": "dottori/s.domenica.jpg"
-  },
-    {
-    "id": 7,
-    "name": "Gaetano",
-    "surname": "Salvo",
-    "email": "g.salvo@balsami.ca",
-    "phonenumber": "02 102983746",
-    "office": 5,
-    "location": 4,
-    "image": "dottori/g.salvo.jpg"
-  },
-    {
-    "id": 8,
-    "name": "Matilde",
-    "surname": "Zampieri",
-    "email": "m.zampieri@balsami.ca",
-    "phonenumber": "02 28364509",
-    "office": 5,
-    "location": 3,
-    "image": "dottori/m.zampieri.jpg"
-  },
-    {
-    "id": 9,
-    "name": "Margherita",
-    "surname": "Saru",
-    "email": "m.saru@balsami.ca",
-    "phonenumber": "02 1209356",
-    "office": 7,
-    "location": 2,
-    "image": "dottori/m.saru.jpg"
-  },
-    {
-    "id": 10,
-    "name": "Benedetto",
-    "surname": "Nespoli",
-    "email": "b.nespoli@balsami.ca",
-    "phonenumber": "02 51029387",
-    "office": 19,
-    "location": 1,
-    "image": "dottori/b.nespoli.jpg"
-  },
-    {
-    "id": 11,
-    "name": "Filippo",
-    "surname": "Calzavara",
-    "email": "f.calzavara@balsami.ca",
-    "phonenumber": "02 20938716",
-    "office": 6,
-    "location": 2,
-    "image": "dottori/f.calzavara.jpg"
-  },
-    {
-    "id": 12,
-    "name": "Giovanni",
-    "surname": "Filaferro",
-    "email": "g.filaferro@balsami.ca",
-    "phonenumber": "02 456782923",
-    "office": 12,
-    "location": 3,
-    "image": "dottori/g.filaferro.jpg"
-  },
-    {
-    "id": 13,
-    "name": "Mauro",
-    "surname": "Mosca",
-    "email": "m.mosca@balsami.ca",
-    "phonenumber": "02 162728392",
-    "office": 23,
-    "location": 4,
-    "image": "dottori/m.mosca.jpg"
-  },
-    {
-    "id": 14,
-    "name": "Alessandra",
-    "surname": "Genovesi",
-    "email": "a.genovesi@balsami.ca",
-    "phonenumber": "02 1267893818",
-    "office": 16,
-    "location": 2,
-    "image": "dottori/a.genovesi.jpg"
-  },
-    {
-    "id": 15,
-    "name": "Mario",
-    "surname": "Apicella",
-    "email": "m.apicella@balsami.ca",
-    "phonenumber": "02 398368449",
-    "office": 21,
-    "location": 1,
-    "image": "dottori/m.apicella.jpg"
-  },
-    {
-    "id": 16,
-    "name": "Michele",
-    "surname": "Buonasera",
-    "email": "m.buonasera@balsami.ca",
-    "phonenumber": "02 5647829309",
-    "office": 12,
-    "location": 4,
-    "image": "dottori/m.buonasera.jpg"
-  },
-    {
-    "id": 17,
-    "name": "Filippo",
-    "surname": "Fratellanza",
-    "email": "f.fratellanza@balsami.ca",
-    "phonenumber": "02 648293843",
-    "office": 16,
-    "location": 3,
-    "image": "dottori/f.fratellanza.jpg"
-  },
-    {
-    "id": 18,
-    "name": "Fernando",
-    "surname": "Aleandro",
-    "email": "f.aleandro@balsami.ca",
-    "phonenumber": "02 23455432",
-    "office": 7,
-    "location": 3,
-    "image": "dottori/f.aleandro.jpg"
-  },
-    {
-    "id": 19,
-    "name": "Sarafino",
-    "surname": "Glugluglu",
-    "email": "s.glugluglu@balsami.ca",
-    "phonenumber": "02 91827372",
-    "office": 8,
-    "location": 1,
-    "image": "dottori/s.glugluglu.jpg"
-  },
-    {
-    "id": 20,
-    "name": "Guido",
-    "surname": "Lamoto",
-    "email": "g.lamoto@balsami.ca",
-    "phonenumber": "02 546372883",
-    "office": 5,
-    "location": 2,
-    "image": "dottori/g.lamoto.jpg"
-  },
-    {
-    "id": 21,
-    "name": "Salvo",
-    "surname": "Pistolo",
-    "email": "s.pistolo@balsami.ca",
-    "phonenumber": "02 29039832",
-    "office": 13,
-    "location": 1,
-    "image": "dottori/s.pistolo.jpg"
-  },
-    {
-    "id": 22,
-    "name": "Domenica",
-    "surname": "Sera",
-    "email": "d.sera@balsami.ca",
-    "phonenumber": "02 234543245",
-    "office": 27,
-    "location": 4,
-    "image": "dottori/d.sera.jpg"
-  },
-    {
-    "id": 23,
-    "name": "Primo",
-    "surname": "Sale",
-    "email": "p.sale@balsami.ca",
-    "phonenumber": "02 2356271873",
-    "office": 2,
-    "location": 1,
-    "image": "dottori/p.sale.jpg"
-  },
-    {
-    "id": 24,
-    "name": "Francesca",
-    "surname": "Alicante",
-    "email": "f.alicante@balsami.ca",
-    "phonenumber": "02 37283932",
-    "office": 29,
-    "location": 2,
-    "image": "dottori/f.alicante.jpg"
-  },
-    {
-    "id": 25,
-    "name": "Dario",
-    "surname": "Lampa",
-    "email": "d.lampa@balsami.ca",
-    "phonenumber": "02 267382912",
-    "office": 4,
-    "location": 3,
-    "image": "dottori/d.lampa.jpg"
-  }
-];
-
 var toOutput="";
 
 /* ----------------- API FORMATS -----------------*/
 
 
-function formatDoctors(item) {
-
-	toOutput+='<div class="col-sm-4"><div class="row cards-small">';
-	toOutput+='<div class="col-sm-3 col-xs-2"><img src="../../assets/img/' + item.image + '" / width="40px"></div>';
-	toOutput+='<div class="col-sm-9 col-xs-10"><b>' + item.name + ' ' + item.surname + '</b><br>'+item.location+'</div>';
-	toOutput+='</div></div>';
-	if(item.id%3==0){
-		toOutput+='</div><div class="row">';
-	}
+function formatDoctorsSmall(item) {
+    toOutput+='<div class="col-sm-4">';
+    toOutput+='<a href="../doctors/id/'+ item.id +'.html">';
+    toOutput+='<div class="row cards-small">';
+    toOutput+='<div class="col-sm-3 col-xs-2"><img src="../../assets/img/' + item.image + '" / width="40px"></div>';       
+    toOutput+='<div class="col-sm-9 col-xs-10"><b>' + item.name + ' ' + item.surname + '</b><br>'+item.location+'</div>';
+    toOutput+='</div></a></div>';
+    if(item.id%3==0){
+        toOutput+='</div><div class="row">';
+    }
 }
+
+
+
+
+function formatDoctorsServices(item) {
+
+    toOutput+='<div class="col-sm-6">';
+    toOutput+='<a class="nodec" href="../doctors/id/'+ item.id +'.html">';
+    toOutput+='<div class="row green-bar">DR.' + item.name + ' ' + item.surname + '</div>';
+    toOutput+='<div class="card">';
+    toOutput+='<div class="col-md-2 col-xs-3"><img src="../../assets/img/' + item.image + '" / width="60px"></div>';       
+    toOutput+='<div class="col-md-10 col-xs-9">Email: '+item.email+'<br>Telefono: '+item.phonenumber+'<br>Ufficio: #'+item.office+', '+item.location+'</div>';
+    toOutput+='</div></a></div>';
+    if(item.id%2==0){
+        toOutput+='</div><div class="row">';
+    }
+
+}
+
+
+
+
+
+
 
 function formatLocations(item) {
     //return '<div class="card">Nome: '+item.name+'</div>';
@@ -301,11 +72,11 @@ $(document).ready(function() {
 function show(what, callback) {
     var levels = what.split("/");
     var level1 = levels[0];
-    /*fetch("/" + what)
+    fetch("/" + what)
         .then(function(response) {
             return response.json();
         })
-        .then(function(data) {*/
+        .then(function(data) {
             var list = $("#list");
             switch (level1) {
                 case "locations":
@@ -316,13 +87,27 @@ function show(what, callback) {
                     break;
                 case "doctors":
                 	toOutput+='<div class="row">';
-                    data.map(formatDoctors);
-                    
+                    level2=levels[1];
+                    switch(level2){
+                        case "services":
+                            data.map(formatDoctorsServices);
+                        case "locations":
+                            data.map(formatDoctorsLocations);
+                        break;
+                        case "id":
+                            //doctor's profile
+                        break;
+                        default:
+                            data.map(formatDoctorsSmall);
+                        break;
+                    }
+                    toOutput+='</div>';
                     break;
                 default:
-                    console.log("NoSuchParameter");
+                    //console.log("NoSuchParameter");
+                    break;
             }
             list.append(toOutput);
-        //});
+        });
 
 }
