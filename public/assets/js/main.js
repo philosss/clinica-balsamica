70cb647a24ade4f5958b5d36b6528e650f6a9968
Filippo Calzavara <filippo@calzavara.me>
2017-06-06 12:09:07 +0200

    Fatte le sedi

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,37 +2,58 @@
 
 
 var toOutput="";
+var iterator=1;
 
 /* ----------------- API FORMATS -----------------*/
 
 
 function formatDoctorsSmall(item) {
-    toOutput+='<div class="col-sm-4">';
+    toOutput+='<div class="col-sm-4 preCards">';
     toOutput+='<a href="../doctors/id/'+ item.id +'.html">';
-    toOutput+='<div class="row cards-small">';
-    toOutput+='<div class="col-sm-3 col-xs-2"><img src="../../assets/img/' + item.image + '" / width="40px"></div>';       
+    toOutput+='<div class="row cards card-small">';
+    toOutput+='<div class="col-sm-3 col-xs-2"><img src="../../assets/img/' + item.image + '" width="40px"></div>';       
     toOutput+='<div class="col-sm-9 col-xs-10"><b>' + item.name + ' ' + item.surname + '</b><br>'+item.location+'</div>';
     toOutput+='</div></a></div>';
-    if(item.id%3==0){
+    if(iterator%3==0){
         toOutput+='</div><div class="row">';
     }
+    iterator++;
 }
-
+function formatLocations(item) {
+    toOutput+='<div class="col-sm-6 preCards">';
+    toOutput+='<a href="../locations/id/'+ item.id +'.html">';
+    toOutput+='<div class="box">';
+    toOutput+='<div class="coverimg"><img src="../../assets/img/cards/' + item.image + '"></div>';       
+    toOutput+='<div class="cityname">' + item.name + '</div>';
+    toOutput+='</div></a></div>';
+    if(iterator%2==0){
+        toOutput+='</div><div class="row">';
+    }
+    iterator++;
+}
 
 
 
 function formatDoctorsServices(item) {
 
-    toOutput+='<div class="col-sm-6">';
-    toOutput+='<a class="nodec" href="../doctors/id/'+ item.id +'.html">';
+
+    toOutput+='<div class="col-sm-6 preCards">';
+    toOutput+='<a href="../doctors/id/'+ item.id +'.html">';
     toOutput+='<div class="row green-bar">DR.' + item.name + ' ' + item.surname + '</div>';
-    toOutput+='<div class="card">';
+    toOutput+='<div class="cards card-big">';
     toOutput+='<div class="col-md-2 col-xs-3"><img src="../../assets/img/' + item.image + '" / width="60px"></div>';       
     toOutput+='<div class="col-md-10 col-xs-9">Email: '+item.email+'<br>Telefono: '+item.phonenumber+'<br>Ufficio: #'+item.office+', '+item.location+'</div>';
     toOutput+='</div></a></div>';
-    if(item.id%2==0){
+    if(iterator%2==0){
         toOutput+='</div><div class="row">';
     }
+    iterator++;
+}
+
+function levelFormatterDoctorsServices(item){
+    console.log("asas");
+    toOutput+='<h1>1</h1>';
+    list.append(data.map(formatDoctorsServices));
 
 }
 
@@ -42,9 +63,6 @@ function formatDoctorsServices(item) {
 
 
 
-function formatLocations(item) {
-    //return '<div class="card">Nome: '+item.name+'</div>';
-}
 
 
 
@@ -78,6 +96,7 @@ function show(what, callback) {
         })
         .then(function(data) {
             var list = $("#list");
+            toOutput+='<div class="row">';
             switch (level1) {
                 case "locations":
                     list.append(data.map(formatLocations));
@@ -86,13 +105,13 @@ function show(what, callback) {
                     list.append(data.map(formatServices));
                     break;
                 case "doctors":
-                	toOutput+='<div class="row">';
+                	
                     level2=levels[1];
                     switch(level2){
                         case "services":
-                            data.map(formatDoctorsServices);
+                            data.map(levelFormatterDoctorsServices);
                         case "locations":
-                            data.map(formatDoctorsLocations);
+                            data.map(levelFormatterDoctorsLocations);
                         break;
                         case "id":
                             //doctor's profile
@@ -101,13 +120,15 @@ function show(what, callback) {
                             data.map(formatDoctorsSmall);
                         break;
                     }
-                    toOutput+='</div>';
+                    
                     break;
                 default:
                     //console.log("NoSuchParameter");
                     break;
             }
+            toOutput+='</div>';
             list.append(toOutput);
+        
         });
 
 }
