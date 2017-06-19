773969c1efd08ca5cb48e0fa1ca6d3843fe8c0c6
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-06-19 17:02:47 +0200

    fix doctors services

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.get("/api/doctors/services", function(req, res) {
                     r[x].doctors.push(results[i]);
                 } else {
                     el = {};
-                    el.service_id = results[i].serviceid;
+                    el.serviceid = results[i].serviceid;
                     el.service_name = results[i].service_name;
                     el.doctors = [results[i]];
                     r.push(el);
